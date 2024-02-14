bfa68aeba04ab7bb31eaca07e12a14d6b5f1d7de
amahadnezhad <amahadnezhad@gmail.com>
2024-02-14 15:37:33 +0330

    Exercise1 -> Create a Class with instance attributes

---
+++ 1.py
@@ -0,0 +1,15 @@
+"""
+Exercise1 -> Create a Class with instance attributes
+
+Write a Python program to create a Vehicle class with max_speed and mileage instance attributes
+"""
+
+
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+
+modelx = Vehicle(240, 18)
+print(modelx.max_speed, modelx.mileage)
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,3 @@
 Hey There!
 In This Repo We're Going To Solve Exercises About Python OOP Concept To Improve Our Python Skills.
+Stay Tuned!
